a55ebf19142a6a51f2842dd6a55e5328646bd1a8
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-12-28 23:00:48 +0530

    Number_inverted_triangle_pattern

---
+++ Number_inverted_triangle_pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 1; i <= 4; i++)
+{
+    for(int j=1; j<i; j++)
+    {
+        Console.Write(" ");
+    }
+    for(int k=4; k>=i; k--)
+    {
+        Console.Write(i);
+    }
+    Console.WriteLine();
+}
