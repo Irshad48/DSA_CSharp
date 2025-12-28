7d4d6fe6e49dec6155e7b53bdbd6cfad42625b56
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-12-28 23:22:51 +0530

    Number_pyramid_pattern

---
+++ Number_pyramid_pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿for (int i = 0; i < 4; i++)
+{
+    for(int j=4; j>i+1; j--)
+    {
+         Console.Write(" ");
+    }
+    for(int k=1; k<=i+1; k++)
+    {
+        Console.Write(k);
+    }
+    for(int l=i; l>=1; l--)
+    {   
+        Console.Write(l);
+    }
+    Console.WriteLine();
+}
