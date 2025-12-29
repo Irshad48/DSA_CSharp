add944ad57f00ac4fd7b362bedb6d8df233b0154
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-12-29 08:37:01 +0530

    Star_butterfly_pattern

---
+++ Number_butterfly_pattern/Program.cs
@@ -0,0 +1,40 @@
+﻿for(int i=0; i<4; i++)
+{
+    for(int j=0; j<i+1; j++)
+    {
+        Console.Write("*");
+    }
+    for (int j = 0; j < 4 - (i + 1); j++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j < 4 - (i + 1); j++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j < (i + 1); j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4 - i; j++)
+    {
+        Console.Write("*");
+    }
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j < 4 - i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
